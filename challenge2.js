218a24d7d69d13ac62e3c56a83fb92023ef91789
Nora Rodriguez <gia.drago4@gmail.com>
2023-10-10 19:43:35 -0600

    challenge3

--- challenge2.js
+++ challenge2.js
@@ -1,1 +1,10 @@
 //palindrome
+
+function palindrome(string){
+    let stringReversed = string.split('').reverse().join("")
+    console.log(stringReversed)
+    if(stringReversed === string) return true
+    return false
+}
+
+console.log(palindrome('anas'))
