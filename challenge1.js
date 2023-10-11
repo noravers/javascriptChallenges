112ac33212b5a2301d84425a78746d94d6e49d67
Nora Rodriguez <gia.drago4@gmail.com>
2023-10-10 19:34:53 -0600

    challenge1

---
+++ challenge1.js
@@ -0,0 +1,34 @@
+const word = 'Greetings'
+
+// function reverseString(string){
+
+//     return string.split('').reverse().join('')
+// } 
+
+// console.log(reverseString(word))
+
+function reverseString2(string){
+    let reversed = ''
+    const stringl = string.length-1
+    for(let i= stringl; i >= 0; i--){
+        reversed = reversed.concat(string[i])
+    }
+    return reversed
+}
+
+console.log(reverseString2(word))
+
+function reverseString3(string){
+    let stringToArray = string.split('')
+    let arrayResult = []
+    for(let i = stringToArray.length -1; i >= 0; i--){
+        arrayResult.push(stringToArray[i])
+    }
+    return arrayResult.join('')
+}
+
+console.log(reverseString3(word))
+
+
+// const experiment = 'string' += 'h'
+// console.log(experiment)
